da275107e29d766d3d6362fc71210aa222cff4b5
Dejan1990 <dejan.simo@hotmail.com>
2024-02-18 17:04:42 +0100

    Extract data to components

--- src/views/TeamView.vue
+++ src/views/TeamView.vue
@@ -1,6 +1,7 @@
 <script setup>
-    import TeamMember from '@/components/Teams/TeamMember.vue';
     import TeamHeader from '@/components/Teams/TeamHeader.vue';
+    import TeamMembers from '@/components/Teams/TeamMembers.vue';
+    import TeamFooter from '@/components/Teams/TeamFooter.vue';
     import team from '@/team.json';
 
     // console.log(team.members[0])
@@ -10,32 +11,8 @@
     <TeamHeader :team="team" />
 
     <div class="place-self-center flex flex-col gap-y-3" style="width: 725px;">
-        <table class="table-fixed border-spacing-2 border-separate">
-            <thead>
-                <th class="text-left px-6 py-2">Name</th>
-                <th class="text-left px-6 py-2">Email</th>
-                <th class="text-left px-6 py-2">Status</th>
-            </thead>
-            <tbody>
-
-                <TeamMember 
-                    v-for="member in team.members" 
-                    :key="member.id" 
-                    :name="member.name" 
-                    :email="member.email" 
-                    :status="member.status" 
-                />
-            </tbody>
-        </table>
-        <p 
-            class="text-right text-gray-600 italic"
-            v-show="team.members.length === team.spots"
-        >
-            There are no remaining team spots. Upgrade to add more.
-        </p>
+        <TeamMembers :team="team" />
     </div>
 
-    <footer class="mt-12 bg-gray-100 py-4 text-center">
-        <h5 class="font-semibold text-lg">{{ team.name }} - {{ team.spots }} Member Team</h5>
-    </footer>
+    <TeamFooter :team="team" />
 </template>
