5b85ac339c1541ddff51916bd29dbc4ff02453bd
Dejan1990 <dejan.simo@hotmail.com>
2024-02-18 16:00:53 +0100

    Show message only if there is no more team spots

--- src/views/TeamView.vue
+++ src/views/TeamView.vue
@@ -10,10 +10,10 @@
     <header class="flex justify-between">
         <div>
             <button 
-                class="bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded disabled:bg-green-400"
-                :disabled="true"
+                class="bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded disabled:bg-gray-400"
+                :disabled="team.members.length === team.spots"
             >
-                Add Member (0 Spots Left)
+                Add Member ({{ team.spots - team.members.length }} Spots Left)
             </button>
         </div>
 
@@ -45,7 +45,12 @@
                 />
             </tbody>
         </table>
-        <p class="text-right text-gray-600 italic">There are no remaining team spots. Upgrade to add more.</p>
+        <p 
+            class="text-right text-gray-600 italic"
+            v-show="team.members.length === team.spots"
+        >
+            There are no remaining team spots. Upgrade to add more.
+        </p>
     </div>
 
     <footer class="mt-12 bg-gray-100 py-4 text-center">
